ac7026bdeb04f1f043a8b3360a656849b57e71e8
Rian-Macedo-Egestao <Rian.macedo@e-gestao.com.br>
2021-10-10 11:16:38 -0300

    adicionado switchs de escolha | typography

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
-import {Container} from '@mui/material'
+import {Container} from '@mui/material';
+import Typography from '@mui/material/Typography';
+import "@fontsource/roboto"
+
 function App() {
   return (
     <div>
       <Container maxWidth="sm">
-        <h1>
-          Formulario de Cadastro
-        </h1>
+        <Typography color="secondary" variant="h3" align="center">Formulário de cadastro</Typography>
         <FormularioCadastro/>
       </Container>
     </div>
   );
 }
-
 export default App;
--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { TextField } from "@mui/material/"
 import {Button} from "@mui/material/"
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 
 class FormularioCadastro extends Component {
     render() { 
@@ -10,7 +12,9 @@ class FormularioCadastro extends Component {
                 <TextField id="sobrenome" label="sobrenome" variant="outlined" fullWidth margin="normal"/>
                 <TextField id="mail" label="email" variant="outlined" fullWidth margin="normal"/>
                 <TextField id="senha" label="senha" variant="outlined" fullWidth margin="normal"/>
-                <Button variant="contained" size="medium">Medium</Button>
+                <FormControlLabel control={<Switch color="success" defaultChecked  name="mailBox" />}label="Receber por email"/>
+                <FormControlLabel control={<Switch color="success" defaultChecked  name="novidadesBox" />}label="Receber novidades"/>
+                <Button  color="success" variant="contained" size="medium">Medium</Button>
 
             </form> 
         );
