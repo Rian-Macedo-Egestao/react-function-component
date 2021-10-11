63a7c66c2b17b62417bb7f5bcde76cebf29f9a34
Rian Macedo <rian.macedo@e-gestao.com.br>
2021-10-11 15:02:17 -0300

    criado funcoes de validacao e submit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,33 @@
 import './App.css';
+import { Component } from 'react';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
 import {Container} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import "@fontsource/roboto"
 
-function App() {
-  return (
-    <div>
-      <Container maxWidth="sm">
-        <Typography color="" variant="h3" align="center">Formulário de cadastro</Typography>
-        <FormularioCadastro/>
-      </Container>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Container maxWidth="sm">
+          <Typography color="" variant="h3" align="center">Formulário de cadastro</Typography>
+          <FormularioCadastro enviar={onSubmit} validar={validarCpf}/>
+        </Container>
+      </div>
+    );
+  }
+}
+
+function onSubmit(dados) {
+  console.log(dados)
+}
+
+function validarCpf(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"o cpf deve ter 11 digitos"}
+  }
+  else {
+    return {valido:true, texto:""}
+  }
 }
 export default App;
--- my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ my-app/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,21 +1,74 @@
-import React  from "react";
+import React, {useState} from "react";
 import { TextField } from "@mui/material/"
 import {Button} from "@mui/material/"
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 
-function FormularioCadastro(){
-    let nome = "";
+function FormularioCadastro({enviar, validar}){
+    const [nome, setNome] = useState("")
+    const [sobrenome, setSobrenome] = useState("")
+    const [cpf, setCpf] = useState("")
+    const [promocoes, setPromocoes] = useState(true)
+    const [noticias, setNoticias] = useState(true)
+    const [error, setError] = useState({cpf:{valido:true, texto:""}})
     
     return (
-        <form>
-            <TextField id="nome" onChange={(event)=>{console.log(event.target.value)}} label="nome" variant="outlined" fullWidth margin="normal"/>
-            <TextField id="sobrenome" label="sobrenome" variant="outlined" fullWidth margin="normal"/>
-            <TextField id="mail" label="email" variant="outlined" fullWidth margin="normal"/>
-            <TextField id="senha" label="senha" variant="outlined" fullWidth margin="normal"/>
-            <FormControlLabel control={<Switch color="success" defaultChecked  name="mailBox" />}label="Receber por email"/>
-            <FormControlLabel control={<Switch color="success" defaultChecked  name="novidadesBox" />}label="Receber novidades"/>
-            <Button  color="success" variant="contained" size="medium">Medium</Button>
+        <form
+        onSubmit={(event)=> {
+            event.preventDefault();
+            enviar({nome, sobrenome, cpf, promocoes, noticias});
+        }}
+        >
+            <TextField id="nome" 
+                value={nome}
+                onChange={(event)=>{
+                    setNome(event.target.value);
+                    }}
+               
+                label="nome" variant="outlined" fullWidth margin="normal"/>
+            <TextField id="sobrenome"
+                value={sobrenome}
+                onChange={(event)=>{
+                        setSobrenome(event.target.value);
+                    }} 
+                label="sobrenome" variant="outlined" fullWidth margin="normal"/>
+
+            <TextField id="cpf" 
+                value={cpf}
+                onChange={(event)=>{
+                    setCpf(event.target.value);
+                    }}
+                onFocus={(event) => 
+                    setError({cpf:{valido:true, texto:""}})
+                }
+                onBlur={(event) => {
+                    const valido = validar(event.target.value);
+                    setError({cpf:valido});
+                }  
+                }
+                error={!error.cpf.valido}
+                helperText={error.cpf.texto}
+                label="cpf" 
+                variant="outlined" 
+                fullWidth 
+                margin="normal"/>
+
+            <FormControlLabel control={<Switch
+            checked={promocoes} 
+            onChange={(event)=> {
+                setPromocoes(event.target.checked);
+            }}
+            color="success"  name="mailBox" />}label="Receber por email"/>
+
+            <FormControlLabel control={<Switch 
+            checked={noticias} 
+            onChange={(event)=> {
+                setNoticias(event.target.checked);
+               
+            }}
+            color="success"  name="novidadesBox" />}label="Receber novidades"/>
+
+            <Button type="submit" color="success" variant="contained" size="medium">Medium</Button>
 
         </form> 
     );
